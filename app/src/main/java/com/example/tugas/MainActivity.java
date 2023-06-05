ddfd456511d906554b86c6c29f504cd139cd7f4b
Muhammad Naufal Faruq <naufalfaruq285@gmail.com>
2023-06-05 11:25:08 +0700

    update UI

--- app/src/main/java/com/example/tugas/MainActivity.java
+++ app/src/main/java/com/example/tugas/MainActivity.java
@@ -1,52 +1,5 @@
 package com.example.tugas;
 
-//import android.os.Bundle;
-//import android.view.View;
-//import android.widget.ArrayAdapter;
-//import android.widget.Button;
-//import android.widget.EditText;
-//import android.widget.ListView;
-//
-//import androidx.appcompat.app.AppCompatActivity;
-//
-//import java.util.ArrayList;
-//
-//public class MainActivity extends AppCompatActivity {
-//
-//    private EditText editTextTask;
-//    private Button buttonAdd;
-//    private ListView listViewTasks;
-//
-//    private ArrayList<String> tasks;
-//    private ArrayAdapter<String> tasksAdapter;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//
-//        editTextTask = findViewById(R.id.editTextTask);
-//        buttonAdd = findViewById(R.id.buttonAdd);
-//        listViewTasks = findViewById(R.id.listViewTasks);
-//
-//        tasks = new ArrayList<>();
-//        tasksAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, tasks);
-//        listViewTasks.setAdapter(tasksAdapter);
-//
-//        buttonAdd.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                String task = editTextTask.getText().toString().trim();
-//                if (!task.isEmpty()) {
-//                    tasks.add(task);
-//                    tasksAdapter.notifyDataSetChanged();
-//                    editTextTask.setText("");
-//                }
-//            }
-//        });
-//    }
-//}
-
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -105,8 +58,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
-
 
 
         // Tambahkan OnClickListener pada tombol "Hapus Semua"
